310823081f6ce9e5558e935bb7f5f22b5a2f8e9d
gopika-gopal <140744560+gopika-gopal@users.noreply.github.com>
2023-12-02 23:18:22 +0530

    second commit

---
+++ Project Task Revenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import businessListStyle from "BusinessList.css";
+import Business from "../Business/Business";
+
+import './BusinessList.css';
+
+class BusinessList extends React.Component {
+    render() {
+
+        return (
+        <div className="BusinessList">
+           {
+            this.props.businesses.map(business =>{
+                return <Business business={business} key={business.id}/>
+            })
+           }
+        </div>
+        )
+    }
+}
+
+export default BusinessList;
